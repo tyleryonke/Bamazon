c24b173477508c86428a6b9087515c62bd2e2235
tyleryonke <tyleryonke@gmail.com>
2017-01-13 14:30:43 -0600

    full complete draft

--- bamazonSchema.sql
+++ bamazonSchema.sql
@@ -8,37 +8,63 @@ CREATE table products (
   department_name VARCHAR(250) NOT NULL,
   price DECIMAL(20, 2) NOT NULL,
   stock_quantity INT NOT NULL,
+  product_sales DECIMAL(20, 2) NOT NULL,
   PRIMARY KEY (item_id)
 );
 
-INSERT INTO products (product_name, department_name, price, stock_quantity)
-VALUES ("Towels", "Bathroom", 24.99, 15);
+INSERT INTO products (product_name, department_name, price, stock_quantity, product_sales)
+VALUES ("Towels", "Bathroom", 24.99, 15, 1);
 
-INSERT INTO products (product_name, department_name, price, stock_quantity)
-VALUES ("Toothpaste", "Bathroom", 7.49, 22);
+INSERT INTO products (product_name, department_name, price, stock_quantity, product_sales)
+VALUES ("Toothpaste", "Bathroom", 7.49, 22, 1);
 
-INSERT INTO products (product_name, department_name, price, stock_quantity)
-VALUES ("Spatula", "Kitchen", 4.95, 9);
+INSERT INTO products (product_name, department_name, price, stock_quantity, product_sales)
+VALUES ("Spatula", "Kitchen", 4.95, 9, 1);
 
-INSERT INTO products (product_name, department_name, price, stock_quantity)
-VALUES ("Cast-Iron Skillet", "Kitchen", 22.50, 11);
+INSERT INTO products (product_name, department_name, price, stock_quantity, product_sales)
+VALUES ("Cast-Iron Skillet", "Kitchen", 22.50, 11, 1);
 
-INSERT INTO products (product_name, department_name, price, stock_quantity)
-VALUES ("Television", "Electronics", 249.99, 4);
+INSERT INTO products (product_name, department_name, price, stock_quantity, product_sales)
+VALUES ("Television", "Electronics", 249.99, 4, 1);
 
-INSERT INTO products (product_name, department_name, price, stock_quantity)
-VALUES ("Rice", "Food/Drink", 6, 188);
+INSERT INTO products (product_name, department_name, price, stock_quantity, product_sales)
+VALUES ("Rice", "Food/Drink", 6, 188, 1);
 
-INSERT INTO products (product_name, department_name, price, stock_quantity)
-VALUES ("Beef Broth", "Food/Drink", 3.99, 19);
+INSERT INTO products (product_name, department_name, price, stock_quantity, product_sales)
+VALUES ("Beef Broth", "Food/Drink", 3.99, 19, 1);
 
-INSERT INTO products (product_name, department_name, price, stock_quantity)
-VALUES ("Coca-Cola", "Food/Drink", 4.5, 30);
+INSERT INTO products (product_name, department_name, price, stock_quantity, product_sales)
+VALUES ("Coca-Cola", "Food/Drink", 4.5, 30, 1);
 
-INSERT INTO products (product_name, department_name, price, stock_quantity)
-VALUES ("Pillowcase", "Bedroom", 12.78, 62);
+INSERT INTO products (product_name, department_name, price, stock_quantity, product_sales)
+VALUES ("Pillowcase", "Bedroom", 12.78, 62, 1);
 
-INSERT INTO products (product_name, department_name, price, stock_quantity)
-VALUES ("Wool Sheets", "Bedroom", 44.99, 13);
+INSERT INTO products (product_name, department_name, price, stock_quantity, product_sales)
+VALUES ("Wool Sheets", "Bedroom", 44.99, 13, 1);
+
+CREATE table departments (
+  department_id INT AUTO_INCREMENT NOT NULL,
+  department_name VARCHAR(250) NOT NULL,
+  over_head_costs DECIMAL(20, 2) NOT NULL,
+  total_sales DECIMAL(20, 2) NOT NULL,
+  PRIMARY KEY (department_id)
+);
+
+INSERT INTO departments (department_name, over_head_costs, total_sales)
+VALUES ("Bathroom", 300, 1);
+
+INSERT INTO departments (department_name, over_head_costs, total_sales)
+VALUES ("Kitchen", 75, 1);
+
+INSERT INTO departments (department_name, over_head_costs, total_sales)
+VALUES ("Electronics", 122.50, 1);
+
+INSERT INTO departments (department_name, over_head_costs, total_sales)
+VALUES ("Food/Drink", 35.25, 1);
+
+INSERT INTO departments (department_name, over_head_costs, total_sales)
+VALUES ("Bedroom", 101.99, 1);
 
 SELECT * FROM products;
+
+SELECT * FROM departments;
---
+++ bamazonSupervisor.js
@@ -0,0 +1,79 @@
+var mysql = require("mysql");
+var inquirer = require("inquirer");
+var Table = require("cli-table");
+
+var connection = mysql.createConnection({
+	host: "localhost",
+	port: 3306,
+
+	user: "root",
+	password: "PASSWORD HERE",
+
+	database: "bamazon"
+});
+
+connection.connect(function(err) {
+  if (err) throw err;
+});
+
+console.log("Welcome to Bamazon! A Beff Jezos Company");
+	
+inquirer.prompt([
+
+{
+	type: "list",
+	message: "Welcome, supervisor. What would you like to do?",	
+	choices: ["View Product Sales by Department", "Create New Department"],
+	name: "choiceOfFunction"
+}
+
+]).then(function(selection) {
+	switch (selection.choiceOfFunction) {
+    	case "View Product Sales by Department":
+	        viewSales();
+        	break;
+    	case "Create New Department":
+        	createDepartment();
+        	break;
+	};
+});
+
+function viewSales() {
+	connection.query("SELECT * FROM departments", function(err, res) {
+		var table = new Table({
+		    head: ["department_id", "department_name", "over_head_costs", "product_sales", "total_profit"],
+		    colWidths: [20, 20, 20, 20, 20]
+		});
+
+		for (var i = 0; i < res.length; i++) {
+			var totalProfit = parseFloat(res[i].total_sales) - parseFloat(res[i].over_head_costs);
+			table.push(
+    			[res[i].department_id, res[i].department_name, res[i].over_head_costs, res[i].total_sales, totalProfit]
+			);
+		}
+
+			 console.log(table.toString());
+	})
+
+	connection.end();
+};
+
+function createDepartment() {
+	inquirer.prompt([
+
+		{
+			type: "input",
+			message: "What is the name of the department you would like to create?",
+			name: "name",
+		}
+
+	]).then(function(selection) {
+
+		connection.query("INSERT INTO departments (department_name, over_head costs, product_sales) VALUES (?, 0, 0);", selection.name, function(err, res) {
+			if (err) throw err;
+			console.log("Department created.")
+		})
+
+		connection.end();
+	})
+};
